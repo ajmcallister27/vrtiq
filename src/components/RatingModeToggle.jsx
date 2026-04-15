4f370c20148538d26cd2041bd5cbe5ffb2a3ffda
Andrew McAllister <ajmcallister27@gmail.com>
2026-04-15 12:04:00 -0700

    updated ski icon

--- src/components/RatingModeToggle.jsx
+++ src/components/RatingModeToggle.jsx
@@ -4,23 +4,7 @@ import { getRatingModeLabel } from '@/lib/ratingMode';
 import { useRatingMode } from '@/lib/RatingModeContext';
 
 function SkiIcon({ className }) {
-  return (
-    <svg viewBox="0 0 1920 1920" aria-hidden="true" className={className}>
-      <style>{`.st0{fill:#fff}.st1{fill:none;stroke:#231f20;stroke-width:50;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10}`}</style>
-      <g>
-        <path className="st0" d="M910 322.1c0-98.2-37.7-105.6-50.7-105.2-2.9.1-5.8.1-8.6 0-13-.4-50.7 6.9-50.7 105.2 0 107.8 11.2 465.9 13.3 631.4.2 16.7.3 31.5.3 43.8 0 26.7-.3 68.6-.9 118.7-2.2 201.4-7.8 534.1-7.8 534.1s-10.7 53 50.1 53c60.7 0 50-53 50-53s-5.5-332.7-7.8-534.1c-.5-50-.9-91.9-.9-118.7 0-12.3.1-27.1.3-43.8 2.2-165.4 13.4-523.6 13.4-631.4z" />
-        <path className="st0" d="M1107.4 1116c-.5-50-.9-91.9-.9-118.7 0-12.3.1-27.1.3-43.8 2.1-165.5 13.3-523.7 13.3-631.4 0-98.2-37.7-105.6-50.7-105.2-2.9.1-5.8.1-8.6 0-13-.4-50.7 6.9-50.7 105.2 0 107.8 11.2 465.9 13.3 631.4.2 16.7.3 31.5.3 43.8 0 26.7-.3 68.6-.9 118.7-2.2 201.4-7.8 534.1-7.8 534.1s-10.7 53 50 53c60.8 0 50-53 50-53s-5.4-332.7-7.6-534.1z" />
-      </g>
-      <g>
-        <path className="st1" d="M905.1 1650.1s10.7 53-50 53-50-53-50-53 5.6-332.7 7.8-534c.5-50 .9-91.9.9-118.6 0-12.3-.1-27.1-.3-43.8-2.3-165.6-13.5-523.8-13.5-631.6 0-98.3 37.7-105.6 50.7-105.2 2.9.1 5.8.1 8.6 0 13-.4 50.7 6.9 50.7 105.2 0 107.8-11.2 465.9-13.3 631.4-.2 16.7-.3 31.5-.3 43.8 0 26.7.3 68.6.9 118.6 2.2 201.5 7.8 534.2 7.8 534.2z" />
-        <path className="st1" d="M1115.1 1650.1s10.7 53-50 53-50-53-50-53 5.6-332.7 7.8-534c.5-50 .9-91.9.9-118.6 0-12.3-.1-27.1-.3-43.8-2.1-165.5-13.3-523.7-13.3-631.4 0-98.2 37.7-105.6 50.7-105.2 2.9.1 5.8.1 8.6 0 13-.4 50.7 6.9 50.7 105.2 0 107.8-11.2 465.9-13.3 631.4-.2 16.7-.3 31.5-.3 43.8 0 26.7.3 68.6.9 118.6 2.1 201.3 7.6 534 7.6 534z" />
-        <path className="st1" d="M1106.8 953.6h-83.4" />
-        <path className="st1" d="M896.7 953.6h-83.4" />
-        <path className="st1" d="M1107.4 1116h-84.6" />
-        <path className="st1" d="M897.3 1116h-84.6" />
-      </g>
-    </svg>
-  );
+  return <img src="/skiing-ski-svgrepo-com.svg" alt="" aria-hidden="true" className={className} />;
 }
 
 function SnowboardIcon({ className }) {
@@ -61,7 +45,7 @@ export default function RatingModeToggle({ className }) {
                 : 'text-slate-500 hover:text-slate-900 hover:bg-white'
             )}
           >
-            <Icon className="w-5 h-5 shrink-0" />
+            <Icon className={cn('w-5 h-5 shrink-0', value === 'ski' && active && 'brightness-0 invert')} />
             <span>{label}</span>
           </button>
         );
